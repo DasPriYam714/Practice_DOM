e8dba054a50c6b0d82acb0cf012003c5be75e598
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-02-21 14:32:52 +0600

    get element by ID and Class

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-console.log("happy man");
+// console.log("happy man");
 
-const liCollection = document.getElementsByTagName("li");
-for ( const li of liCollection ) {
-    console.log(li.innerText);
-}
+// const liCollection = document.getElementsByTagName("li");
+// for ( const li of liCollection ) {
+//     console.log(li.innerText);
+// }
+
+
+//get element BY ID
+let bannerElement = document.getElementById('headline');
+console.log(bannerElement.textContent);
+bannerElement.textContent='SUBSCRIPTION NEWSLINE'
+
+let headerElement = document.getElementById('headline');
+console.log (headerElement.innerText);
+
+
+headerElement.style.color = 'blue';
+
+// Get elements by Class name
+
+console.log(document.getElementsByClassName('descrip-red')[1]);
+
+let classID = document.getElementsByClassName('descrip-red')
+console.log(classID.innerText);
+
+
